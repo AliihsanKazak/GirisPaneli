5f4cca5a8e7a9ff2327aaf44001ea73b4722bdbc
Ali İhsan Kazak <ali.kazak.ihsan@gmail.com>
2024-02-26 23:52:39 +0300

    GirisPaneli

---
+++ Login_Panel/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Login_Panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn_giris = new System.Windows.Forms.Button();
+            this.kullaniciadi = new System.Windows.Forms.TextBox();
+            this.sifre = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(136, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 134);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_giris);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(170, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(448, 426);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kullanıcı Girişi";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.kullaniciadi);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(136, 183);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(174, 62);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kullanıcı Adı";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.sifre);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(136, 251);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(174, 62);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Şifre";
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_giris.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_giris.Location = new System.Drawing.Point(136, 319);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(174, 33);
+            this.btn_giris.TabIndex = 0;
+            this.btn_giris.Text = "Giriş Yap";
+            this.btn_giris.UseVisualStyleBackColor = false;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click);
+            // 
+            // kullaniciadi
+            // 
+            this.kullaniciadi.Location = new System.Drawing.Point(6, 25);
+            this.kullaniciadi.Name = "kullaniciadi";
+            this.kullaniciadi.Size = new System.Drawing.Size(162, 26);
+            this.kullaniciadi.TabIndex = 3;
+            // 
+            // sifre
+            // 
+            this.sifre.Location = new System.Drawing.Point(6, 25);
+            this.sifre.Name = "sifre";
+            this.sifre.PasswordChar = '●';
+            this.sifre.Size = new System.Drawing.Size(162, 26);
+            this.sifre.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_giris;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox sifre;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox kullaniciadi;
+    }
+}
+
---
+++ Login_Panel/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Panel
+{
+    public partial class Form1 : Form
+    {
+
+        string ka = "Ali";
+        string sfr = "Sa1234";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_giris_Click(object sender, EventArgs e)
+        {
+
+
+            if(ka == kullaniciadi.Text && sfr==sifre.Text)
+            {
+                MessageBox.Show("GİRİS YAPILDI");
+            }
+            else
+            {
+                MessageBox.Show("GİRİS YAPILMADI");
+            }
+        }
+    }
+}
